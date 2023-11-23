39dfd3ab7eaa5377a64a9f1cfa2e4bb70ebb8165
ULTRA-L <loydvendiola@gmail.com>
2023-11-23 22:21:36 +0800

    Initial Commit

---
+++ DataPreperation.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import re
+from sentence_transformers import SentenceTransformer
+
+df_imdb = pd.read_csv("CSV/IMDBdata_MainData.csv")
+df_imdb = df_imdb[['Title','Released','Genre','Director','Writer','Actors','Plot','Language','Country','Production']]
+
+#Filter
+def filtering(column:str):
+  df_imdb[column]=df_imdb[column].astype(str)
+  for i in range(len(df_imdb[column])):
+    str_tmp = df_imdb[column][i]
+    str_tmp = str_tmp.replace(",","")
+    df_imdb[column][i] = str_tmp
+
+filtering('Genre')
+filtering('Actors')
+filtering('Director')
+filtering('Country')
+
+#Cleanup Writer
+regex = r"\(([^\)]+)\)"
+df_imdb['Writer']=df_imdb['Writer'].astype(str)
+
+for i in range(len(df_imdb['Writer'])):
+  writer = df_imdb['Writer'][i]
+  matches = re.finditer(regex,writer,re.MULTILINE)
+  for matchNum, match in enumerate(matches, start=1):
+    writer = writer.replace(match.group(),"")
+  writer = writer.replace(",", "")
+  df_imdb['Writer'][i] = writer
+
+df_imdb.dropna(inplace=True)
+df_imdb.to_csv('CSV/filter_imdb.csv')
+df = pd.read_csv('CSV/filter_imdb.csv')
+df.drop(columns = df.columns[0], axis = 1, inplace=True)
+
+def combine_features(row):
+    return row['Title']+' '+row['Released']+' '+ row['Genre']
+    +' '+row['Director']+' '+row['Writer']+' '+row['Actors']+' '+row['Plot']
+    +' '+row['Language']+' '+row['Country']+' '+row['Production']
+
+df = df.astype(str)
+df['combined_value'] = df.apply(combine_features,axis=1)
+df['index'] = [i for i in range(0,len(df))]
+
+bert = SentenceTransformer('bert-base-nli-mean-tokens')
+sentence_embeddings = bert.encode(df['combined_value'].tolist())
+
+df_bert = pd.DataFrame(sentence_embeddings)
+df_bert.to_csv("CSV/bert.csv")
---
+++ main.py
@@ -0,0 +1,104 @@
+import streamlit as st
+import pandas as pd
+import DataPreperation as dp
+from sklearn.metrics.pairwise import cosine_similarity
+from openai import OpenAI
+
+df = pd.read_csv('CSV/filter_imdb.csv')
+df.drop(columns = df.columns[0], axis=1, inplace = True)
+df = df.astype(str)
+df['combined_value'] = df.apply(dp.combine_features,axis=1)
+df['index'] = [i for i in range(0,len(df))]
+
+sentence_embeddings = pd.read_csv('CSV/bert.csv')
+df.drop(columns = df.columns[0], axis=1, inplace = True)
+
+similarity = cosine_similarity(sentence_embeddings)
+indices = pd.Series(df['Title'])
+
+def recommend(title, cosine_sim = similarity):
+  recommended_movies = []
+  if indices[indices == title].empty:
+    return None
+  idx = indices[indices == title].index[0]
+  score_series = pd.Series(cosine_sim[idx]).sort_values(ascending = False)
+  top_5_indices = list(score_series.iloc[1:6].index)
+  for i in top_5_indices:
+    recommended_movies.append(list(df['Title'])[i])
+  return recommended_movies
+
+client = OpenAI(
+    api_key = st.secrets["BERT_OPEN_AI_KEY"]
+)
+
+def reply(query):
+  top5 = recommend(query)
+  if top5 is not None:
+    top5_str = ""
+    for i in top5:
+      top5_str += i + ","
+
+    template = f"""
+      The given prompt may be answer using the top 5 list 
+      {top5_str}
+      """
+  else:
+    template = f"""
+      But it seems that the prompt isn't recognizable by the database.
+      So you suggest a movie title close to the prompt
+      """
+
+  completion = client.chat.completions.create(
+    model='gpt-3.5-turbo-0301',
+    messages=[
+      {
+        "role": "system",
+        "content":
+          """
+            You are a Movie Recommender Chatbot named MovieBuddy.
+            Your purpose is to recommend atleast top 5 Movies according to the user's response
+            You usually reply in a friendly manner
+          """ + template
+      },
+      {
+        "role": "user",
+        "content": query
+      }
+    ]
+  )
+
+  response = completion.choices[0].message.content
+  return response
+
+st.title("BERT Movie Recommender")
+
+if "messages" not in st.session_state:
+  st.session_state.messages = []
+
+for message in st.session_state.messages:
+  with st.chat_message(message["role"]):
+    st.markdown(message["content"])
+
+if prompt := st.chat_input("Enter a Movie Title"):
+  st.chat_message("user").markdown(prompt)
+
+  st.session_state.messages.append(
+    {
+      "role": "user",
+      "content": prompt
+    }
+  )
+
+  responses = reply(prompt)
+
+  with st.chat_message("assistant"):
+    for response in responses:
+      st.markdown(f"BERT: {response}")
+
+  for response in responses:
+    st.session_state.messages.append(
+      {
+        "role": "assistant",
+        "content": f"Echo: {response}"
+      }
+    )
